275eaca964b9f55d6726a61336d8ecd40aac3c9b
kaytee <https://github.com/akglitch/kayteeestate.git>
2023-06-16 03:59:01 +0000

    car.js

---
+++ models/car.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const User = require('./User');
+
+const carSchema = new Schema({
+  make: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  mileage: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Car = mongoose.model('Car', carSchema);
+
+module.exports = Car;
